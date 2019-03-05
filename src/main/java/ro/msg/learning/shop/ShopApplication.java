0d0c13980f23f492e0fc19f6fe1dc31a566733c3
czimermi <ivett.czimerman@msg.group>
2019-03-05 11:39:37 +0200

    Removed unused import statements

--- src/main/java/ro/msg/learning/shop/ShopApplication.java
+++ src/main/java/ro/msg/learning/shop/ShopApplication.java
@@ -3,8 +3,6 @@ package ro.msg.learning.shop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import ro.msg.learning.shop.model.Location;
-import ro.msg.learning.shop.model.Stock;
 
 @SpringBootApplication
 @EnableJpaRepositories
